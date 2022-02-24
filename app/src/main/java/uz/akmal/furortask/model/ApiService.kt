1e58dd066d151456f0d325fb81f0c9791a171c6e
Dilnoza <icebear03051999@gmail.com>
2022-02-24 08:16:50 +0500

    config

--- app/src/main/java/uz/akmal/furortask/model/ApiService.kt
+++ app/src/main/java/uz/akmal/furortask/model/ApiService.kt
@@ -1,11 +1,12 @@
 package uz.akmal.furortask.model
 
 import retrofit2.Response
+import retrofit2.http.GET
 import retrofit2.http.POST
 import uz.akmal.furortask.model.data.response.GetItemResponse
 
 interface ApiService {
-    @POST("api/product")
+    @GET("api/product")
     suspend fun getItemsList(): Response<List<GetItemResponse>>
 
 }
